039ae7ce12b44ee775f7037253b92eece8874bff
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-22 12:33:28 -1000

    passed 63rd test (forte)

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1087,7 +1087,7 @@ var nemo = new Animal('Fish', gender.male);
 
 // Create 2 different vehicles
 var civic = new Vehicle('Honda', 'Civic');
-var forte;
+var forte = new Vehicle('KIA', 'Forte');
 
 // Create 2 shapes with different numbers of sides
 var square;
