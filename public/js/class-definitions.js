568ca339af888d50b15cce88783589ffe66dc941
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-22 14:13:14 -1000

    passed 86th test (findShoes)

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1236,6 +1236,9 @@ Door.prototype.openClose = function() {
  *
  */
 
+Shoe.prototype.findShoes = function() {
+  return "Found " + this.color + " shoes of size " + this.size;
+};
 
  /* Step 87
  *
