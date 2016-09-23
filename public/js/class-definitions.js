ac422840c73c2a7c93deef1591720711f90c0465
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-22 14:36:06 -1000

    passed 97th test

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1384,8 +1384,8 @@ var blackShoesSize10 = dressShoes.findShoes();
  * and assign the values to each variable below.
  *
  */
-var farTooTallAStory;
-var shortStory;
+var farTooTallAStory = twoStory.isATallStory(1);
+var shortStory = singleStory.isATallStory(3);
 
 
  /* Step 98
